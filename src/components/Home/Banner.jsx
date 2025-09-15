ffede75d7ba89d82fa8fc700202a7172aa9bf5c5
hirabatool191 <batool.hira2020@gmail.com>
2025-09-15 09:06:21 -0700

    push request

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import { RiDoubleQuotesL } from "react-icons/ri";
 const Banner = () => {
   return (
     <div className="md:py-20 py-10 px-4">
       <div className="container mx-auto text-white text-center">
-       
         <h1 className="hidden lg:block text-3xl md:text-5xl font-bold  mb-4">
           <span className="typewriter delay-0">
             I’m Hira — React developer passionate
@@ -34,6 +33,11 @@ const Banner = () => {
           </span>
         </h1>
         <p>Where creativity meets clean code & modern design.</p>
+        <div className="relative border border-white/50 w-fit p-5 mx-auto mt-5 md:mt-10 text-white font-bold">
+          <RiDoubleQuotesL className="absolute -top-3 left-3 text-2xl text-purple-400" />
+
+          <p className="pl-2">Crafting modern web magic.</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,3 +10,5 @@ const Home = () => {
 }
 
 export default Home
+
+//https://www.figma.com/design/lgHVCKSbr7YLZGq2WvHd4a/Portfolio--Community-?node-id=0-1&p=f&t=PdohUnPmJp0sIGbt-0
